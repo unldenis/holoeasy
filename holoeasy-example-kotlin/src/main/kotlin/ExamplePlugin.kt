edfdab0af03c998d60b1e7a27fa38959074f4075
unldenis <mehillideny13@gmail.com>
2024-04-14 20:58:36 +0200

    to 3.2.0

---
+++ holoeasy-example-kotlin/src/main/kotlin/ExamplePlugin.kt
@@ -0,0 +1,41 @@
+package org.holoeasy.plugin
+
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
+import org.holoeasy.HoloEasy
+import org.holoeasy.builder.HologramBuilder.*
+
+class ExamplePlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        val pool = HoloEasy.startInteractivePool(this, 60.0, 0.5f, 5f)
+
+
+        getCommand("hologram")?.setExecutor { sender, _, _, _ ->
+
+            val location = (sender as Player).location
+
+            pool.registerHolograms {
+
+                hologram(location) {
+                    val clickCount = mutableStateOf(0) // can be any type
+
+                    textline("Hello")
+                    textline("Count {}", clickCount)
+                    clickable("Click me").onClick { player -> clickCount.set(clickCount.get() + 1) }
+                    item(ItemStack(Material.APPLE))
+                }
+
+
+                // other holograms...
+
+            }
+
+
+            true
+        }
+    }
+
+}
--- holoeasy-example/src/main/java/org/holoeasy/plugin/ExamplePlugin.java
+++ holoeasy-example/src/main/java/org/holoeasy/plugin/ExamplePlugin.java
@@ -1,14 +1,16 @@
 package org.holoeasy.plugin;
 
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.holoeasy.HoloEasy;
-import org.holoeasy.config.HologramKey;
 import org.holoeasy.pool.IHologramPool;
 import org.holoeasy.reactive.MutableState;
+
+
 import static org.holoeasy.builder.HologramBuilder.*;
 
 public class ExamplePlugin extends JavaPlugin {
@@ -16,17 +18,29 @@ public class ExamplePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         IHologramPool pool = HoloEasy.startInteractivePool(this, 60, 0.5f, 5f);
+
+
         getCommand("hologram").setExecutor((sender, cmd, s, args) -> {
 
-            Player player = (Player) sender;
-            MutableState<Integer> clickCount = mutableStateOf(0); // can be any type
+            Location location = ((Player) sender).getLocation();
 
-            hologram(new HologramKey(pool, "unique-id-holo"), player.getLocation(), () -> {
-                textline("Hello");
-                textline("Count {}", clickCount);
-                clickable("Click me").onClick(p -> clickCount.set(clickCount.get() + 1));
-                item(new ItemStack(Material.APPLE));
+
+            pool.registerHolograms(() -> {
+
+                hologram(location, () -> {
+                    MutableState<Integer> clickCount = mutableStateOf(0); // can be any type
+
+                    textline("Hello");
+                    textline("Count {}", clickCount);
+                    clickable("Click me").onClick(p -> clickCount.set(clickCount.get() + 1));
+                    item(new ItemStack(Material.APPLE));
+                });
+
+                // other holograms...
+
             });
+
+
 
             return true;
         });
